c7877d497a2d4505f2adb85d0d87ed3d59fb11fc
qliugithub <761244895@qq.com>
2019-07-18 21:34:00 +0800

    Site updated: 2019-07-18 21:33:58

--- js/post-details.js
+++ js/post-details.js
@@ -1,5 +1,17 @@
 /* global NexT, CONFIG */
 
+
+$(document).ready(function(){
+    $(document).on('click', '.fold_hider', function(){
+        $('>.fold', this.parentNode).slideToggle();
+        $('>:first', this).toggleClass('open');
+    });
+    //默认情况下折叠
+    $("div.fold").css("display","none");
+});
+
+
+
 $(document).ready(function() {
 
   function initScrollSpy() {
